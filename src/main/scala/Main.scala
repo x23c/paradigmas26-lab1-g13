a970e560401e8544a0c5ad4abfe9ed930258d3d2
Francisco Asis <francisco.asis@mi.unc.edu.ar>
2026-03-25 21:44:44 -0300

    Esta implementado el filtro y cuando ejecuto funciona no se como probarlo mejor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,8 +9,12 @@ object Main {
     val allPosts: List[(String, String)] = subscriptions.map { case (name, url) =>
       println(s"Fetching posts from: $url")
       val stringFeed = FileIO.downloadFeed(url)
-      val posts = Parser.parseRedditFeed(stringFeed)
+      val posts = Parser.parseRedditFeed(stringFeed).filter { case (_, title, selftext, _) =>
+        title.nonEmpty && selftext.nonEmpty && selftext.trim.nonEmpty
+      }
       (url, posts.map(Formatters.formatPost).mkString("\n\n"))
+      //Aca filtre el resultado del parser con la funcion de alto orden filter 
+      //y los 3 parametros q puse son en orden para cuando no tiene titulo, no tiene texto y cuando el texto es vacio
     }
 
     val output = allPosts
